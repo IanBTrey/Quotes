c9cfdd39109d9f9e1dce1b7563ad58d2bbca008c
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-04-24 22:55:57 +0300

    Add changes

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,9 @@
 export class Quotes {
-   public showDescription:boolean;
-    constructor(public id:number, public name:string,public description:string,public completeDate:Date){
-      this.showDescription=false
+  public showAuthor:boolean;
+    public showSubmit:boolean;
+    constructor(public id:number, public name:string, public author:string, public submit:string, public upvotes:number, public downvotes:number, public quoteDate:Date){
+      this.showAuthor=false;
+      this.showSubmit=false;
     }
-}
+
+  }

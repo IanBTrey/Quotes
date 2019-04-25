4a8188f49e4661c43eb97cb2520d95d85d0d06ed
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-04-25 10:58:01 +0300

    style it up

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -7,15 +7,16 @@ export class StrikethroughDirective {
 
   constructor(private elem:ElementRef){}
 
+
   @HostListener("click") onClicks(){
     this.textDeco("line-through")
-  }
-
-  @HostListener("dblclick") onDoubleClicks(){
-   this.textDeco("None")
  }
 
-    private textDeco(action:string){
-    this.elem.nativeElement.style.textDecoration='line-through';
-  }
+ @HostListener("dblclick") onDoubleClicks(){
+  this.textDeco("None")
+ }
+
+   private textDeco(action:string){
+   this.elem.nativeElement.style.textDecoration='line-through';
+  } 
 }
